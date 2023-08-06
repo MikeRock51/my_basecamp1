7892ff482b2a28dbc39e5af08ca17c0593dd6cd1
MikeRock51 <mikerockmusic51@gmail.com>
2023-08-06 15:16:36 +0100

    Implement user auth route

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -45,6 +45,13 @@ def createUser():
             return make_response(jsonify(
                 {"Error": f"{field} is missing"}), 400)
 
+    allUsers = storage.all(User)
+
+    for user in allUsers.values():
+        if user.email == userInfo['email']:
+            return make_response(jsonify({
+                "Error": "An account with this email already exist"}), 400)
+
     hashedPassword = bcrypt.hashpw(userInfo['password'].encode('UTF-8'),
                                    bcrypt.gensalt())
     userInfo['password'] = str(hashedPassword, "UTF-8")
@@ -52,3 +59,33 @@ def createUser():
     user.save()
 
     return make_response(jsonify(user.toDict()), 201)
+
+
+@app_views.route("/users/auth", methods=["POST"], strict_slashes=False)
+def authenticateUser():
+    """Authenticates user credentials"""
+    authData = request.get_json()
+
+    if not authData:
+        return make_response(jsonify({"Error": "Not a JSON"}), 400)
+
+    requiredFields = ["email", "password"]
+
+    for field in requiredFields:
+        if field not in authData:
+            return make_response(jsonify({"Error": f"{field} is missing"}))
+
+    user = storage.getByEmail(authData['email'])
+
+    if not user:
+        abort(404)
+
+    knownPwd = user.password.encode('UTF-8')
+    authPwd = authData['password'].encode('UTF-8')
+
+    if bcrypt.checkpw(authPwd, knownPwd):
+        user = user.toDict()
+        user['loggedIn'] = True
+        return make_response(jsonify(user), 200)
+    
+    return make_response(jsonify({"Error": "Incorrect password"}), 401)
