c62e8a6f2b85d3b8411484e399136d96714aa564
MikeRock51 <mikerockmusic51@gmail.com>
2023-08-11 06:16:02 +0100

    Update Sign up page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,35 +4,25 @@ import {
   Routes,
   // useNavigate,
 } from "react-router-dom";
-import './App.css';
-import SignIn from './components/pages/signIn';
+import "./App.css";
+import SignIn from "./components/pages/SignIn";
 import SignUp from "./components/pages/SignUp";
-
+import Home from "./components/pages/Home";
 
 function App() {
   return (
     <Router>
-    <div className="App position-relative py-5 px-2">
-      <div className="mb-5 pb-4">
-        <div className="nav-container container-fluid rounded pe-0 fixed-top">
+      <div className="App position-relative py-5 px-2">
+        <div className="mb-5 pb-4">
+          <div className="nav-container container-fluid rounded pe-0 fixed-top"></div>
         </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/sign-in" element={<SignIn />} />
+        </Routes>
       </div>
-      <Routes>
-        <Route
-          path="/sign-up"
-          element={
-            <SignUp/>
-          }
-        />
-        <Route
-          path="/sign-in"
-          element={
-            <SignIn />
-          }
-        />
-      </Routes>
-    </div>
-  </Router>
+    </Router>
   );
 }
 
